37518436bcc73615a7ecbcbd5c16d998c6ead78b
Justin Lazarski <blackhawks189@gmail.com>
2019-03-27 20:57:36 -0700

    First Commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+/*
+Justin Lazarski
+3/27/19
+CIS 3090.04
+In Class Activity 5
+ */
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
